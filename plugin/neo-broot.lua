d5ef6432ddbf86393e08b29f2b9767d79897a58f
Sam O'Brien <sam.obrien@ni.com>
2024-10-29 10:11:51 -0500

    Multiple commands at once

--- plugin/neo-broot.lua
+++ plugin/neo-broot.lua
@@ -5,7 +5,7 @@ function open_broot_in(path)
     local win_width = math.ceil(vim.api.nvim_get_option("columns") * 0.90 - 2)
     local col = math.ceil((vim.api.nvim_get_option("columns") - win_width) * 0.5)
     local row = math.ceil((vim.api.nvim_get_option("lines") - win_height) * 0.5 - 1)
-	
+
 	local tmp_file = os.tmpname()
 	local cmd = "broot --conf " .. broot_conf_path .. " --verb-output " .. tmp_file .. " " .. path
 	local buffer = vim.api.nvim_create_buf(false, true)
@@ -18,8 +18,8 @@ function open_broot_in(path)
 		style = "minimal"
 	})
   vim.api.nvim_buf_set_keymap(buffer, "t", "<Esc>", "<Esc>", {silent = true})
-	
-	vim.fn.termopen(cmd, { on_exit = function() 
+
+	vim.fn.termopen(cmd, { on_exit = function()
 	  vim.api.nvim_win_close(window, true)
     vim.api.nvim_buf_delete(buffer, {force = true})
 	  local file = io.open(tmp_file, "r")
@@ -27,23 +27,25 @@ function open_broot_in(path)
 	  local content = file:read("*a")
 	  file:close()
 	  os.remove(tmp_file)
-	  local command, file = string.match(content, "^(%w+) (.+)$")
-	  if command == "edit" then
-	    vim.cmd.edit(file)
-	  elseif command == "cd" then
-	    vim.cmd.cd(file)
-    elseif command == "tabedit" then
-      vim.cmd.tabedit(file)
-    elseif command == "split" then
-      vim.cmd.split(file)
-    elseif command == "vsplit" then
-      vim.cmd.vsplit(file)
-	  else
-	    error("Unknown command " .. command)
-	  end
-	end })
+	  for command, file in string.gmatch(content, "(%w+) ([^\r\n]+)") do
+      print("broot " .. command .. " " .. file)
+      if command == "edit" then
+        vim.cmd.edit(file)
+      elseif command == "cd" then
+        vim.cmd.cd(file)
+      elseif command == "tabedit" then
+        vim.cmd.tabedit(file)
+      elseif command == "split" then
+        vim.cmd.split(file)
+      elseif command == "vsplit" then
+        vim.cmd.vsplit(file)
+      else
+        error("Unknown command " .. command)
+      end
+    end
+  end })
   vim.api.nvim_command("startinsert")
 end
-  
+
 vim.api.nvim_create_user_command('Broot', function() open_broot_in('.') end, {})
 vim.api.nvim_create_user_command('BrootFile', function() open_broot_in(vim.fn.expand("%:h")) end, {})
